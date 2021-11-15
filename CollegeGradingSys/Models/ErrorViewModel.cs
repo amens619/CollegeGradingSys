63dafdcae5b9f9d03690e92bf56ff6e6a29715ea
Amens200 <amens619@gmail.com>
2021-11-15 11:15:17 +0300

    Add project files.

---
+++ CollegeGradingSys/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CollegeGradingSys.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
