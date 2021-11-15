a83a77b380b2d51b8b0fa1f4ca4b7007ff8c6b71
Amens200 <amens619@gmail.com>
2021-11-15 13:39:09 +0300

    Add CollegeRepository

---
+++ CollegeGradingSys/Models/Repositories/CollegeRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollegeGradingSys.Models.Repositories
+{
+    public class CollegeRepository : ICollegeGradingSysRepository<College>
+    {
+        IList<College> colleges;
+        public CollegeRepository()
+        {
+            colleges = new List<College>()
+            {
+                new College{  Id =1,CollegeName="الشريعة"}
+            };
+        }
+
+        public void Add(College entity)
+        {
+            var college = colleges.FirstOrDefault();
+
+            entity.Id = college != null ? colleges.Max(b => b.Id) + 1 : 1;
+
+
+            colleges.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var college = Find(id);
+            colleges.Remove(college);
+        }
+
+        public College Find(int id)
+        {
+            return colleges.SingleOrDefault(a => a.Id == id);
+        }
+
+        public IList<College> List()
+        {
+            return colleges;
+        }
+
+        public void Update(int id, College newAuthor)
+        {
+            var oldCollege = Find(id);
+            oldCollege.CollegeName = newAuthor.CollegeName;
+        }
+    }
+}
