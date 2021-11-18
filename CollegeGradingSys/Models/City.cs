930ec8af4799f28025a0280f0e407b18e56d7d89
Amens200 <amens619@gmail.com>
2021-11-18 17:12:04 +0300

    add City Model

--- CollegeGradingSys/Models/City.cs
+++ CollegeGradingSys/Models/City.cs
@@ -6,19 +6,16 @@ using System.Threading.Tasks;
 
 namespace CollegeGradingSys.Models
 {
-    public class District
+    public class City
     {
         [Key]
         public int Id { get; set; }
 
         [Required]
         [StringLength(60, MinimumLength = 3)]
-        [Display(Name = "المديرية/المنطقة")]
-        public string DistrictName { get; set; }
+        [Display(Name = "المدينة")]
+        public string CityName { get; set; }
 
-
-        public Governorate Governorate { get; set; }
-
-        public ICollection<City> Cities  { get; set; }
+        public District District { get; set; }
     }
 }
